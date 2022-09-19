bd4c2f59a859006a8ecb5017cfeb8c3a9c66465f
Felix Prima <felix.prima@binus.ac.id>
2022-09-19 21:52:41 +0700

    1-Routing

---
+++ study-laravel/resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>About</title>
+</head>
+<body>
+    <h2>Halaman About</h2>
+
+    <h3> <?= $name ?> </h3> 
+    <h4> <?= $email ?></h4>
+</body>
+</html>
---
+++ study-laravel/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome'); // Ketika akses halaman akan menjalan function di dalamnya. return view > tampilkan sebuah view yang namanya welcome
+// });
+
+Route::get('/', function () {
+    return view('Halaman Home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Felix Prima",
+        "email" => "felix.prima@binus.ac.id"
+    ]); 
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
